519e414b495ec5d4ac72f46cdcd0192122519b52
winnd <AS40879@yeah.net>
2021-04-20 16:53:04 +0800

    更新依赖及打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    mode: 'development',
+    // mode: 'development',
+    base: './',
     server: {
         port: 3020,
         open: '/',
+    },
+    build: {
+        rollupOptions: {
+            input: resolve(__dirname, 'index.html'),
+        },
     },
 })
